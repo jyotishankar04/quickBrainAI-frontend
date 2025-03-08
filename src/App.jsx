4782a642d509095d33acd25a703916abbb15f730
jyotishankar04 <patrajyotishankar@gmail.com>
2025-03-08 20:30:09 +0530

    adding sidebar navbar and foooter

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,34 @@ import Login from './app/_auth/pages/Login.jsx'
 import NotFoundPage from './components/NotFoundPage.jsx'
 import OtpVerification from './app/_auth/pages/OtpVerification.jsx'
 import OnBoard from './app/_auth/pages/OnBoard.jsx'
+import RootLayout from './app/_root/RootLayout.jsx'
+import Dashboard from './app/_root/pages/Dashboard.jsx'
+import Favotires from './app/_root/pages/Favorites.jsx'
+import MyNotes from './app/_root/pages/MyNotes.jsx'
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/auth/*'  element={<AuthLayout/>} >
+      <Route path='/auth'  element={<AuthLayout/>} >
         <Route path='login' element={<Login/>} />
         <Route path='register' element={<SignUp/>} />
         <Route path='otp-verification' element={<OtpVerification/>} />
         <Route path='onboard' element={<OnBoard />} />
-       <Route path='*' element={<NotFoundPage />} />
+      <Route path='*' element={<NotFoundPage />} />
 
       </Route>
-      <Route path='/app' element={<Outlet />}  >
-      
+      <Route path='/app' element={<RootLayout />}  >
+        <Route path='' element={<Dashboard />} /> 
+        <Route path='mynotes' element={<MyNotes/>} />
+        <Route path='favorites' element={<Favotires />} />    
+        <Route path='shared' element={<div>Shared</div>} />
+        <Route path='profile' element={<div>Profile</div>} />
+        <Route path='settings' element={<><Outlet /></>}>
+              <Route path='general' element={<div>General</div>} />
+              <Route path='security' element={<div>Security</div>} />
+        </Route>
+
 
       </Route>
 
