0c4dc2d0092de23176cc8928d71899cdf61ebfcf
sarojkumar <sarojojha2457@gmail.com>
2025-03-17 11:13:11 +0530

    Added React pdf feature

--- src/components/_root/workspace/RIghtTab.jsx
+++ src/components/_root/workspace/RIghtTab.jsx
@@ -2,19 +2,21 @@ import useActiveTab from "../../../context/ActiveTabContext";
 import ChatBot from "./ChatBot";
 import PDFViewer from "./PDFViewer";
 import WorkSpaceRIghtTopBar from "./WorkSpaceRIghtTopBar";
+import { PDFViewer as ReactPdfViewer } from "@react-pdf/renderer";
 
-const RIghtTab = () => {
+const RightTab = () => {
   const { activeTab } = useActiveTab();
   return (
-    <div className="w-full flex flex-col gap-2 border-l">
+    <div className="w-full flex flex-col gap-2 border-l h-full">
       <WorkSpaceRIghtTopBar />
+
       <div className="w-full flex-1 flex justify-center items-center">
         {activeTab === 0 ? (
-          <PDFViewer
-            url={
-              "https://res.cloudinary.com/djby1yfko/image/upload/v1741511569/CSR_fsl6cp.pdf"
-            }
-          />
+          <div style={{ width: '100%', height: '100%' }}>
+            <ReactPdfViewer width="100%" height="100%">
+              <PDFViewer />
+            </ReactPdfViewer>
+          </div>
         ) : (
           <ChatBot />
         )}
@@ -23,4 +25,4 @@ const RIghtTab = () => {
   );
 };
 
-export default RIghtTab;
+export default RightTab;
