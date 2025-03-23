b7649c202788100725448afcf57e1a51a99ab34d
jyotishankar04 <patrajyotishankar@gmail.com>
2025-03-23 21:30:19 +0530

    fix/bugs

--- src/components/_root/notes/NotesSection.jsx
+++ src/components/_root/notes/NotesSection.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
